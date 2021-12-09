45af87e458f8f50428d6ec5bf2daa1fe574aa4e6
val <valentin@schoolmouv.fr>
2021-12-09 09:29:00 +0100

    fix(big-numbers): fix return types

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -2,11 +2,11 @@ import { CustomOperation } from './custom-number.types'
 
 export type BigNumber = any
 
-const fromString = (s: string) => undefined
+const fromString = (s: string): BigNumber => undefined
 
-const add = (n1: BigNumber, n2: BigNumber) => undefined
+const add = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
 
-const multiply = (n1: BigNumber, n2: BigNumber) => undefined
+const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
 
 const toString = (n: BigNumber) => ''
 
